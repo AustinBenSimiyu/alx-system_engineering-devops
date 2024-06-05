37cd025eef9a721382c9af2df00af44f53a0ef6b
AustinBenSimiyu <rubberducktoo@gmail.com>
2024-06-05 23:45:35 +0300

    Made some changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-""" This script queries the Reddit API and returns the number of subscribers"""
+"""function that queries the Reddit API and returns the
+number of subscribers for a given subreddit"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ queries the Reddit API and returns the number of subscribers"""
-
-    res = requests.get("https://www.reddit.com/r/{}/about.json"
-                       .format(subreddit))
-    if res.status_code >= 300:
+    """function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit"""
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
+    else:
         return 0
-
-    return res.json().get("data").get("subscribers")
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-""" Queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit."""
+"""function that queries the reddit API and prints the
+titles of the first 10 hot posts listed for a given subreddit."""
+
+
 import requests
 
 
 def top_ten(subreddit):
-    """ queries the Reddit API and prints the titles
-    of the first 10 hot posts listed for a given subreddit."""
-
-    res = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                       .format(subreddit))
-    if res.status_code >= 300:
-        print('None')
-    else:
-        for child in res.json().get("data").get("children"):
-            print(child.get('data').get('title'))
+    """function that queries the reddit API and prints the
+        titles of the first 10 hot posts listed for
+        a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    try:
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            children = response.json().get('data').get('children')
+            for i in range(10):
+                print(children[i].get('data').get('title'))
+        else:
+            print("None")
+    except Exception:
+        print("None")
